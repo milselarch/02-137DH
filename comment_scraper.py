7daaeafcd098749be7e47f79b211d967912f8098
treemanw1 <wee.neville@ymail.com>
2022-04-16 23:50:20 +0800

    Completed comment scraper

--- comment_scraper.py
+++ comment_scraper.py
@@ -18,13 +18,14 @@ def scrape_comments(subreddit, comment_limit, no_posts):
         for top_level_comment in post.comments:
             if isinstance(top_level_comment, praw.models.MoreComments):
                 continue
-            ls.append(post)
+            ls.append(top_level_comment.body)
+            print(top_level_comment.body)
         df = pd.DataFrame(ls)
-        df.to_csv('democrat100comments.csv', index=False)
+        df.to_csv('republican100comments.csv', index=False)
 
 
 reddit_read_only = praw.Reddit(client_id="Yq5U6qZg9bdz2hpfG_maWw",  # your client id
                                client_secret="VBlf2B7eWmM27gt0CilvSLx_kQPI_Q",  # your client secret
                                user_agent="HASS Scraping")  # your user agent
-subreddit = reddit_read_only.subreddit("democrat")
+subreddit = reddit_read_only.subreddit("republican")
 scrape_comments(subreddit, 5, 100)

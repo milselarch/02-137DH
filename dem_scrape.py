d4fb0dc7b155be6fc9cbc442d6add685123cccea
milselarch <milselarch@gmail.com>
2022-04-24 13:05:57 +0800

    scrape r/democrats, not r/democrat

--- dem_scrape.py
+++ dem_scrape.py
@@ -2,7 +2,7 @@ from comment_scraper import *
 
 max_posts = 5000
 max_comments = 250
-rep_subreddit = reddit_read_only.subreddit("democrat")
+rep_subreddit = reddit_read_only.subreddit("democrats")
 scrape_comments(
     rep_subreddit, max_comments, max_posts,
     export_name='dem-comments'
